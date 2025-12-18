b09161e7dd3ff0d98a2ac6ef4347cc5a1b18eb69
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-12-18 23:34:21 +0530

    your commit message

--- server.js
+++ server.js
@@ -7,13 +7,33 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL ||       'https://otp-auth-system-frontend-5v89.vercel.app',
-    
-    ],
-    credentials: true
-}));
+const corsOptions = {
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'https://otp-auth-system-frontend-5v89.vercel.app',
+            process.env.FRONTEND_URL,
+            'http://localhost:3000',
+            'http://localhost:3001'
+        ].filter(Boolean);
+
+        if (!origin) return callback(null, true);
+        
+        if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    maxAge: 600 // 10 minutes
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
